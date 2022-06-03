a0ad016d40ed1f27cecf56214a53b0f0d2772c97
andmosc <chita75ru@mail.ru>
2022-06-03 20:36:14 +0900

    основная задача выполнена

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,29 @@ import java.util.Scanner;
 
 public class Main {
     public static final int NUM_FLURS = 25;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Queue<Integer> queue = new LinkedList<>();
         int input;
 
-        while(true) {
+        while (true) {
             System.out.print("Ожидаю ввода этажа (для завершения надмите 0) : ");
             input = Integer.parseInt(scanner.nextLine());
 
-            if (input >   NUM_FLURS || input < 0)
+            if (input > NUM_FLURS || input < 0)
                 continue;
             if (input == 0) {
                 outFlurs(queue);
                 return;
             }
-
             queue.offer(input);
         }
     }
+
     public static void outFlurs(Queue q) {
-        while(!q.isEmpty()) {
-            System.out.print("этаж "+q.poll()+" -> ");
+        while (!q.isEmpty()) {
+            System.out.print("этаж " + q.poll() + " -> ");
         }
         System.out.println(" этаж 0");
     }
